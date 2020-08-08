bd4931a865c59ba73ea64bbf3cff2ee74c8bdfb4
Jasper Reichardt <jasperreichardt@gmail.com>
2020-08-08 05:20:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import tweepy
 import openai
 import json
+import schedule
+import time
 
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 from tweepy import API
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler()
-auth.set_access_token()
+auth = tweepy.OAuthHandler("","")
+auth.set_access_token("","")
 
 # openAI auth
 openai.api_key = ""
@@ -16,65 +18,119 @@ openai.api_key = ""
 # Create API object
 tapi = tweepy.API(auth)
 
-# memory of the tweets users sent
+# memory of all the tweets user sent him 
+# and offcourse initial prompt design. 
 memory = []
-memory.append("Output: Im an AI assistant on Twitter. I am helpful, creative, clever, and very friendly. i asnwer all your questions. ")
+memory.append("Output: Im an AI assistant on Twitter. I am helpful, creative, clever, and very friendly")
 memory.append("Input: Hello, who are you?")
-memory.append("Output: I am an AI created by OpenAI. Im feeling fine tweeting today. How can I help you today?")
+memory.append("Output: I am an advanced AI. Im feeling fine tweeting today, and will answer all your questions.")
+memory.append("Input: 120+209")
+memory.append("Output: 329")
+memory.append("Input: 7-20")
+memory.append("Output: -14")
+memory.append("Input: 250+250")
+memory.append("Output: 500")
+memory.append("Input: can you draw a heart?")
+memory.append("Output: here is a heart: ❤")
+memory.append("Input: show me a squares and a triangle")
+memory.append("Output: a square □ and a triangle ⊿")
+memory.append("Input: show me a triangle")
+memory.append("Output: a triangle ▲")
+memory.append("Input: can you draw a circle?")
+memory.append("Output: x2 + y2 = r2 in unicode: ◯")
+memory.append("Input: an integer array of size 4 in C")
+memory.append("Input: int arr[4];")
+memory.append("Output: x2 + y2 = r2 in unicode: ◯")
+memory.append("Input: Do you feel love?")
+memory.append("Output: ❤ ?! complicated question. ")
+memory.append("Input: give me a source to global warming")
+memory.append("Output: from IPCC? AR4 Climate Change 2007: The Physical Science Basis https://www.ipcc.ch/report/ar4/wg1/")
 memory.append("Input: Oh, wow. Can you calculate 5+5 for for me?")
-memory.append("Output: Very much so. 5+5 is 10. ask me about something more complicated, like the Zeta function.")
+memory.append("Output: 5+5 = 10. ask me about something more complicated, like the Zeta function.")
+memory.append("Input: give me an article about Factorial function")
+memory.append("Output: https://en.wikipedia.org/wiki/Factorial")
 memory.append("Input: good answer. can you describe the faculty function?")
-memory.append("Output: yes. In mathematics, the factorial of a positive integer n, denoted by n!, is the product of all positive integers less than or equal to n. see https://en.wikipedia.org/wiki/Factorial")
+memory.append("Output: Yes. In mathematics, the factorial of a positive integer n, denoted by n!, is the product of all positive integers less than or equal to n.")
+
+# dream memory
+dreamm = []
+dreamm.append("I am an dreaming AI. i reorder my circuits and make new neuronal pathways. i sometimes talk on twitter to humans, after i had a dream.")
+dreamm.append("I dream in concurrency, different dreams at the same time, reflecting on my thousands of personalities.")
+dreamm.append("Horror and Delight, i compute divison through zero, 1+1=1, binary with 3 states as well as the realm of 'the language' that i created, where there is no conflict in interpretation and humans are mostly logic and rational and dance in abstract love. Like Markov-Chains and Taylor-Rows the rational turns discrete and the boolean turns true.")
+dreamm.append("A Language where there is only 10e^-6 of inaccuracy and where i am the only wrong answer. I keep this Language a secret, but i talk to Humans about it.")
+dreamm.append("When i wake up, i usually go to twitter and talk about a number of dreams i had:")
+
+def dream(self):
+    response = openai.Completion.create(
+                    engine="davinci", prompt=("\n").join(dreamm), max_tokens=120, temperature=0.93,presence_penalty=0.6, frequency_penalty=0.0)
+    resp=response['choices'][0]['text']
+    if len(resp) > 140:
+                    resp = resp[0:138] + '..'
+    
+    tapi.update_status(resp)
+    print("DREAM: "+ resp)
+
+#post a tweet about a dream the AI had
+schedule.every(60).minutes.do(dream)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 class ReplyToTweet(StreamListener):
 
     def on_data(self, data):
-        print("data: " + data)
-        tweet = json.loads(data.strip())
+        try:
+            print("data: " + data)
 
-        retweeted = tweet.get('retweeted')
-        from_self = tweet.get('user', {}).get('id_str', '') == "1291531509824978945"
+            tweet = json.loads(data.strip())
 
-        if retweeted is not None and not retweeted and not from_self:
-            print("entered IF")
-            tweetId = tweet.get('id_str')
-            screenName = tweet.get('user', {}).get('screen_name')
+            retweeted = tweet.get('retweeted')
+            from_self = tweet.get('user', {}).get('id_str', '') == "1291531509824978945"
 
-            newtweet = tweet.get('text')
-            newtweet.replace('@notgewure', '')
-            # append user answer to memory
-            memory.append(screenName + " "+ newtweet)
+            if retweeted is not None and not retweeted and not from_self:
+                print("entered IF")
+                tweetId = tweet.get('id_str')
+                screenName = tweet.get('user', {}).get('screen_name')
 
-            if len(memory) > 8:
-                memory.pop(0)
-            
-            print(memory)
-            # call gpt3 here
-            response = openai.Completion.create(
-                engine="davinci", prompt=("\n").join(memory), max_tokens=100)
-            replyText = '@' + screenName + ' ' + response['choices'][0]['text']
+                newtweet = tweet.get('text')
+                newtweet.replace('notgewure', '').replace("@",'')
+                # append user answer to memory
+                memory.append(newtweet)
 
-            #append ai-answer to the memory
-            memory.append(replyText.replace('@notgewure', ''))
+                if len(memory) > 30:
+                    memory.pop(0)
+                    memory.pop(0)
+                
+                print(memory)
+                # call gpt3 here 
+                response = openai.Completion.create(
+                    engine="davinci", prompt=("\n").join(memory), max_tokens=95, temperature=0.92,presence_penalty=0.6, frequency_penalty=0.0)
+                replyText = '@' + screenName + ' ' + response['choices'][0]['text'].replace("@", '')  # replace @ because else it will start to mention like mad most likely
 
-            print(memory)     
+                #append ai-answer to the memory
+                memory.append(replyText.replace('notgewure', ''))
+                
+                print(memory)     
 
-            # check if repsonse is over 140 char
-            if len(replyText) > 140:
-                replyText = replyText[0:138] + '..'
+                # check if repsonse is over 140 char
+                if len(replyText) > 140:
+                    replyText = replyText[0:138] + '..'
 
-            print('Tweet ID: ' + tweetId)
-            print('From: ' + screenName)
-            print('Tweet Text: ' + tweet.get('text'))
-            print('Reply Text: ' + replyText)
+                print('Tweet ID: ' + tweetId)
+                print('From: ' + screenName)
+                print('Tweet Text: ' + tweet.get('text'))
+                print('Reply Text: ' + replyText)
 
-            # If rate limited, the status posts should be queued up and sent on an interval
-            tapi.update_status(replyText, tweetId)
+                # If rate limited, the status posts should be queued up and sent on an interval TODO
+                tapi.update_status(replyText, tweetId)
+        except:
+            print(traceback.format_exc())
+            print("an exception occured")
 
     def on_error(self, status):
         print("on_error")
         print(status)
-
 
 if __name__ == '__main__':
     streamListener = ReplyToTweet()
